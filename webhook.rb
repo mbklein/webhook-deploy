f06ed243c19dfefc1dac4d8e4ab03688a0fa37b9
Michael Klein <mbklein@gmail.com>
2016-11-11 11:48:06 -0600

    Configure from environment; verify webhook secret

--- webhook.rb
+++ webhook.rb
@@ -8,9 +8,14 @@ class DeployWebhook < Sinatra::Base
   logger filename: "log/#{settings.environment}.log"
   
   configure do
-    set :refs, ['refs/heads/develop']
-    set :deploy_dir, '/var/www/webhook-deploy/tmp/avalon'
-    set :logfile, '/var/www/webhook-deploy/log/avalon_deploy.log'
+    set :refs, ENV['WEBHOOK_REFS'].split(/,\s*/)
+    set :deploy_dir, ENV['WEBHOOK_DIR']
+    set :logfile, ENV['WEBHOOK_LOG']
+  end
+  
+  before do
+    request.body.rewind
+    @payload_body = request.body.read
   end
   
   helpers do
@@ -20,6 +25,11 @@ class DeployWebhook < Sinatra::Base
         system "git fetch && git checkout #{branch} && git pull origin #{branch} >> #{settings.logfile} 2>&1"
       end
     end
+    
+    def verify_signature!
+      signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), ENV['WEBHOOK_SECRET'], @payload_body)
+      return halt 403, "Signatures didn't match!" unless Rack::Utils.secure_compare(signature, request.env['HTTP_X_HUB_SIGNATURE'])
+    end
   end
   
   get '/' do
@@ -27,11 +37,11 @@ class DeployWebhook < Sinatra::Base
   end
   
   post '/' do
+    verify_signature!
     event = request.env['HTTP_X_GITHUB_EVENT'].to_s
     logger.info "Received event: `#{event}`"
     if request.env['HTTP_X_GITHUB_EVENT'] == 'push'
-      request.body.rewind
-      payload = JSON.parse(request.body.read)
+      payload = JSON.parse(@payload_body)
       logger.info "Ref: #{payload['ref']}"
       if settings.refs.include?(payload['ref'])
         branch = payload['ref'].split('/',3).last
